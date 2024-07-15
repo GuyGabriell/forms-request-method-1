03aba26b448793e85cf3bb1428e4272e827c3544
GuyGabriell <jonahgabriel85@yahoo.com>
2024-07-15 18:26:14 +0100

    added a tailwinduicss

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,9 +6,14 @@
 
 $heading = 'Create Note';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+//dd($_SERVER);
 
-  ($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  dd($_POST);
 }
+
+
+
 
 require 'views/note-create.view.php';
